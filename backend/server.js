c9429dc97ef2ac1f17d959e5a34307b02f44d765
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2023-09-16 17:55:20 +0000

    first route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.get("/api/goals", (req, res) => {
-  res.json({ message: "Get goals" });
+  res.status(200).json({ message: "Get goals" });
 });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
