0b42d80a77ee3a9acdcd9c6a7c9ef31ff498b035
Mallikarjuna Annigeri <125271707+mallikarjuna-nirmaan@users.noreply.github.com>
2023-09-18 11:51:02 +0530

    j

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "Get goals" });
+// });
 
-module.exports = router;
+// module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(express.json());
 
-app.use("/api/goals", require("./routes/goalRoutes"));
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+});
+
+// app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
