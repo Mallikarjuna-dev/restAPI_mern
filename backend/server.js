41ffcca985080a77e12d18aee32834b4d1fbb30c
Mallikarjuna Annigeri <mallikarjunaannigeri27@gmail.com>
2023-09-18 16:14:19 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
