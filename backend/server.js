e3204ef0ab1df750d3b2a4eb33d4fab3450aedb0
Mallikarjuna Annigeri <125271707+mallikarjuna-nirmaan@users.noreply.github.com>
2023-09-18 12:24:14 +0530

    vc set

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get goalsa" });
+  res.status(200).json({ message: "Get goals" });
 });
 
 // app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.listen(port, () => console.log(`server started on port ${port}`))
+app.listen(port, () => console.log(`server started on port ${port}`));
