f25f6557ad809f37b35e425b818c1d8512ebe4a2
Mallikarjuna Annigeri <125271707+mallikarjuna-nirmaan@users.noreply.github.com>
2023-09-18 12:09:17 +0530

    test11

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  res.status(200).json({ message: "Get goals a" });
 });
 
 // app.use("/api/goals", require("./routes/goalRoutes"));
