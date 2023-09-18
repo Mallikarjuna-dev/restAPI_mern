50c4e3b408808c55ebe7b574941aed65c8c962ea
Mallikarjuna Annigeri <125271707+mallikarjuna-nirmaan@users.noreply.github.com>
2023-09-18 16:18:08 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json());
-
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get goalss" });
-});
-
-// app.use("/api/goals", require("./routes/goalRoutes"));
+// app.use(express.json());
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
