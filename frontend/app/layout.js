4cd8e0d5c7ed83a94408d92535935f102320a79e
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2023-09-26 12:34:23 +0530

    frontend redux setup

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
+import { store } from '@/redux/store'
 import { Inter } from 'next/font/google'
+import { Provider } from 'react-redux'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider store={store}>{children}</Provider>
+      </body>
     </html>
   )
 }
